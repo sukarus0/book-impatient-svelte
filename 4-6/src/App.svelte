00f1e6b4c56ba954e8efb606cf6d2797a83de9e9
sukarus0 <sukarus@gamil.com>
2022-07-21 07:45:23 +0000

    Done 4-6

---
+++ 4-6/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	async function promiseFunc() {
+		const result = new Promise((resolve, reject) => {
+			setTimeout(()=>{
+				resolve("Hello Universe~!");
+			}, 1000);
+		});
+		const rtn = await result;
+		return rtn;
+	}
+	let myPromise = promiseFunc();
+</script>
+
+<main>
+	{#await myPromise}
+		<h1> 잠시만 기다리세요.. </h1>
+	{:then res}
+		<h1> {res} </h1>
+	{:catch error}
+		<h1> Error: {error.message} </h1>
+	{/await}
+
+	<hr>
+	<button on:click={()=> myPromise = promiseFunc() }> async 함수 호출하기 </button>
+</main>	
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
