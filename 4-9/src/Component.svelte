9b2e136e6587d021d1b41cceb020a57989a3897f
sukarus0 <sukarus@gamil.com>
2022-07-21 14:49:56 +0000

    Done 4-9

---
+++ 4-9/src/Component.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {afterUpdate, beforeUpdate, onDestroy, onMount} from "svelte"
+	onMount(()=>console.log("Component 생성됨"));
+	onDestroy(()=>console.log("Component 파괴됨"));
+
+	export let name;
+</script>
+
+<p>
+	hello {name}
+</p>
