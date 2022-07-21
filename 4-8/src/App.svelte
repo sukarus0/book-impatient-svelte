7a3598c4ffdbffcc9dfefd6959299c5e9cca237b
sukarus0 <sukarus@gamil.com>
2022-07-21 14:30:13 +0000

    Done 4-8

---
+++ 4-8/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let name = 'world';
+</script>
+
+<main>
+	<input type=text bind:value={name}>
+
+	<p>{Date()}</p>
+
+	{#key name}
+		<p>{Date()}</p>
+		<h1>Hello {name}!</h1>
+	{/key}
+</main>	
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
